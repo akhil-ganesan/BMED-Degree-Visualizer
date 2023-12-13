278b530b1628be8eacecc0ab37b1d87627b273fb
Akhil Ganesan <Strker74@gmail.com>
2023-12-13 12:17:00 -0500

    Added General Institute Requirements

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,19 @@
-prereq_chain = {"MATH 1552": ["MATH 1551"],
+prereq_chain = {
+    "APPH 1040/1050/1060": [],
+    "ENGL 1101": [], 
+    "ENGL 1102": ["ENGL 1101"],
+    "MATH 1552": ["MATH 1551"],
     "CS 1371": [],
+    "Humanities 1": [],
+    "Humanities 2": [],
     "PHYS 2211": ["MATH 1552"],
     "PHYS 2212": ["PHYS 2211"],
     "MATH 1551": [],
     "MATH 1553": ["MATH 1551"],
+    "HIST 2111/HIST 2112/INTA 1200/POL 1101/PUBP 3000": [],
+    "Social Sciences 1": [],
+    "Social Sciences 2": [],
+    "Social Sciences 3": [],
     "BMED 1000": [],
     "BMED 2110": ["CHEM 1211K", "MATH 1552", "BMED 1000"],
     "COE 2001": ["MATH 1552", "PHYS 2211"],
@@ -25,7 +35,9 @@ prereq_chain = {"MATH 1552": ["MATH 1551"],
     "BMED 2400": ["MATH 1552", "CS 1371"],  
     "MSE 2001": ["CHEM 1211K"],
     "ECE 3710": ["PHYS 2212"],
-    "ECE 3741": ["ECE 3710"]
+    "ECE 3741": ["ECE 3710"], 
+    "Depth Electives": [],
+    "Breadth Electives": []
 }
 
 def courseVisualizer(coursework):
